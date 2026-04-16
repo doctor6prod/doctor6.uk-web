0ec11806156691ae5a5443cfc4b93166d3fadaa3
doctor6prod <doctor6prod@users.noreply.github.com>
2026-04-15 23:25:21 -0300

    sdc

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
-import { routes } from './router' // Cambié ~/ por ./ para probar
+import { routes } from './router'
 import 'virtual:windi.css'
 
 export const createApp = ViteSSG(
   App,
   { routes },
   ({ router }) => {
-    // Sin lógica extra para probar
+    // Evita que Vue Router intercepte enlaces externos
+    router.beforeEach((to, from, next) => {
+      if (to.fullPath.startsWith('http')) {
+        window.location.href = to.fullPath
+      } else {
+        next()
+      }
+    })
   }
 )
